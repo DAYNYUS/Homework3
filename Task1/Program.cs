156b9d7ef917d68abfc020a79fa03c562464be0a
unknown <excaliburbmw@mail.ru>
2022-06-20 17:40:34 +0200

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите прогамму, которая принимает на вход пятизначное число и проверяет,
+//является ли оно полиндромом (14212 -> нет; 23432 -> да; 12821 -> да)
+
+Console.Write("Введите пятизначное число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  
+
+bool IsItPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (IsItPalindrome()) Console.WriteLine("да");
+else Console.WriteLine("нет");
+Console.WriteLine();
